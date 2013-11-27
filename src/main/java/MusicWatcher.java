ae2e345e3e1cefa4cc4e2b1ce87c972e5cef852c
Matthias Bollwein <matthias.bollwein@tngtech.com>
2013-11-28 00:42:26 +0100

    refactoring

--- src/main/java/MusicWatcher.java
+++ src/main/java/MusicWatcher.java
@@ -1,4 +1,6 @@
 import com.tngtech.configbuilder.ConfigBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -7,86 +9,118 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 public class MusicWatcher {
-    public static void main(String[] args){
 
-        MusicWatcherConfig config = new ConfigBuilder<>(MusicWatcherConfig.class).withCommandLineArgs(args).build();
-        Map<WatchKey,Path> keys = new HashMap<>();
-        Map<Path,Set<Path>> folders = new HashMap<>();
+    private final static Logger log = LoggerFactory.getLogger(MusicWatcher.class);
 
+    private MusicWatcherConfig config;
+    private Map<WatchKey,Path> keys = new HashMap<>();
+    private Map<Path,Set<Path>> folders = new HashMap<>();
+    private WatchService watchService;
+
+    public void watch(String[] args){
         try {
-            WatchService watchService = config.getSourceDir().getFileSystem().newWatchService();
-            WatchKey key = config.getSourceDir().register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
-            keys.put(key,config.getSourceDir());
-
-            for(;;) {
-               WatchKey watchKey = watchService.poll(60, TimeUnit.SECONDS);
-               if(watchKey == null && !folders.isEmpty()) {
-                   FilesProcessor filesProcessor = new FilesProcessor(new HashMap<>(folders), config);
-                   Thread t = new Thread(filesProcessor);
-                   t.start();
-                   folders.clear();
-               }
-               else if(watchKey != null) {
-                  List<WatchEvent<?>> events = watchKey.pollEvents();
-                  for (WatchEvent event : events) {
-                      Path dir = keys.get(watchKey);
-                      if (dir == null) {
-                          System.err.println("WatchKey not recognized!");
-                          continue;
-                      }
-                      Path newPath = dir.resolve((Path)event.context());
-                      System.out.println(event.kind().toString() + ": " + newPath);
-                      if(!newPath.toFile().isDirectory() && newPath.toString().toLowerCase().endsWith(".mp3")) {
-                          System.out.println(newPath + " is mp3 file");
-                          if(!folders.containsKey(dir)) {
-                              folders.put(dir, new HashSet<Path>());
-                          }
-                          folders.get(dir).add(newPath);
-                      }
-                      else if(newPath.toFile().isDirectory()) {
-                          System.out.println(newPath + " is directory");
-                          registerAll(newPath, keys, watchService);
-
-                      }
-                  }
-                  if (!watchKey.reset()) {
-                      keys.remove(watchKey);
-                      if (keys.isEmpty()) {
-                          break;
-                      }
-                  }
-               }
-            }
+            initialize(args);
+            waitForAndProcessEvents();
         } catch (Exception e) {
-            System.out.println("Exception");
+            log.warn("Exception");
         }
     }
 
-    private static void registerAll(final Path start, final Map<WatchKey,Path> keys, final WatchService watchService) throws IOException {
-        // register directory and sub-directories
+    private void initialize(String[] args) throws IOException {
+        config = new ConfigBuilder<>(MusicWatcherConfig.class).withCommandLineArgs(args).build();
+        watchService = config.getSourceDir().getFileSystem().newWatchService();
+        WatchKey key = config.getSourceDir().register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
+        keys.put(key,config.getSourceDir());
+    }
+
+    private void waitForAndProcessEvents() throws InterruptedException, IOException {
+        for(;;) {
+           WatchKey watchKey = watchService.poll(60, TimeUnit.SECONDS);
+           if(watchKey == null && !folders.isEmpty()) {
+               processFilesInNewThread();
+           }
+           else if(watchKey != null) {
+               if (!watchKeyIsRecognizedAndProcessed(watchKey)) continue;
+               if (noWatchKeysLeftAfterResettingOrRemoving(watchKey)) break;
+           }
+        }
+    }
+
+    private void processFilesInNewThread() {
+        FilesProcessor filesProcessor = new FilesProcessor(new HashMap<>(folders), config);
+        Thread t = new Thread(filesProcessor);
+        t.start();
+        folders.clear();
+    }
+
+    private boolean watchKeyIsRecognizedAndProcessed(WatchKey watchKey) throws IOException {
+        List<WatchEvent<?>> events = watchKey.pollEvents();
+        Path dir = keys.get(watchKey);
+        if (dir == null) {
+            log.warn("WatchKey not recognized!");
+            return false;
+        }
+        else {
+            processEvents(events, dir);
+            return true;
+        }
+    }
+
+    private void processEvents(List<WatchEvent<?>> events, Path dir) throws IOException {
+        for (WatchEvent event : events) {
+            Path newPath = dir.resolve((Path)event.context());
+            log.debug(event.kind().toString() + ": " + newPath);
+            if(!newPath.toFile().isDirectory() && newPath.toString().toLowerCase().endsWith(".mp3")) {
+                log.debug(newPath + " is mp3 file");
+                addMp3FileToMap(dir, newPath);
+            }
+            else if(newPath.toFile().isDirectory()) {
+                log.debug(newPath + " is directory");
+                registerDirAndSubDirs(newPath, keys, watchService);
+            }
+        }
+    }
+
+    private void addMp3FileToMap(Path dir, Path newPath) {
+        if(!folders.containsKey(dir)) {
+            folders.put(dir, new HashSet<Path>());
+        }
+        folders.get(dir).add(newPath);
+    }
+
+    private void registerDirAndSubDirs(final Path start, final Map<WatchKey, Path> keys, final WatchService watchService) throws IOException {
         Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                register(dir, keys, watchService);
+                registerDir(dir, keys, watchService);
                 return FileVisitResult.CONTINUE;
             }
         });
     }
 
-    private static void register(Path dir, Map<WatchKey,Path> keys, WatchService watchService) throws IOException {
+    private void registerDir(Path dir, Map<WatchKey, Path> keys, WatchService watchService) throws IOException {
         WatchKey key = dir.register(watchService, ENTRY_CREATE, ENTRY_MODIFY);
         Path prev = keys.get(key);
         if (prev == null) {
-            System.out.format("register: %s\n", dir);
+            System.out.format("registerDir: %s\n", dir);
         } else {
             if (!dir.equals(prev)) {
                 System.out.format("update: %s -> %s\n", prev, dir);
             }
         }
         keys.put(key, dir);
+    }
+
+    private boolean noWatchKeysLeftAfterResettingOrRemoving(WatchKey watchKey) {
+        if (!watchKey.reset()) {
+            keys.remove(watchKey);
+            if (keys.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ src/main/java/WatcherMain.java
@@ -0,0 +1,7 @@
+
+public class WatcherMain {
+    public static void main(String[] args){
+        MusicWatcher musicWatcher = new MusicWatcher();
+        musicWatcher.watch(args);
+    }
+}
